e9e1bca356d55b7b8affa4e99f8a31824152eaf2
Shuvam Singh <shuvam77@gmail.com>
2021-11-03 18:11:28 +0100

    first commit ch4

---
+++ pages/tests.py
@@ -0,0 +1,42 @@
+from django.conf.urls import url
+from django.http import response
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse, resolve
+
+from pages.views import HomePageView
+
+# Create your tests here.
+
+
+class HomePageTest(SimpleTestCase):
+
+    def setUp(self):
+        url = reverse('home')
+        self.response = self.client.get(url)
+
+    def test_homepage_status_code(self):
+        # response = self.client.get('/')
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_url_name(self):
+        # response = self.client.get(reverse('home'))
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_template(self):
+        # response = self.client.get('/')
+        self.assertTemplateUsed(self.response, 'home.html')
+
+    def test_homepage_contains_correct_html(self):
+        # response = self.client.get('/')
+        self.assertContains(self.response, 'Homepage')
+
+    def test_homepage_does_not_contains_incorrect_html(self):
+        # response = self.client.get('/')
+        self.assertNotContains(self.response, 'Hi there! I should not be here.')
+
+    def test_homepage_url_resolves_homepageview(self):
+        view = resolve('/')
+        self.assertEqual(
+            view.func.__name__,
+            HomePageView.as_view().__name__
+        )
---
+++ users/tests.py
@@ -0,0 +1,36 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+# Create your tests here.
+
+
+class CustomUserTests(TestCase):
+
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username='prakash',
+            email='prakash@gmail.com',
+            password='shahi123'
+        )
+
+        self.assertEqual(user.username, 'prakash')
+        self.assertEqual(user.email, 'prakash@gmail.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username='superadmin',
+            email='superadmin@gmail.com',
+            password='shahi123'
+        )
+
+        self.assertEqual(admin_user.username, 'superadmin')
+        self.assertEqual(admin_user.email, 'superadmin@gmail.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
