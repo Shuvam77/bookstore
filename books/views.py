11717ba5b94afab2fd57369318965992da52b241
shuvam77 <shuvam77@gmail.com>
2022-03-24 13:32:39 +0100

    ch15

--- books/views.py
+++ books/views.py
@@ -1,4 +1,6 @@
+from turtle import title
 from django.db import models
+from django.db.models import Q
 
 from django.contrib.auth.mixins import (
     LoginRequiredMixin,
@@ -25,3 +27,15 @@ class BookDetailView(LoginRequiredMixin,PermissionRequiredMixin ,DetailView):
     template_name = 'books/book_detail.html'
     login_url = 'account_login'
     permission_required = 'books.special_status'
+
+class SearchResultsListView(ListView):
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'books/search_result.html'
+    # queryset = Book.objects.filter(title__icontains='professionals')
+
+    def get_queryset(self):
+        query = self.request.GET.get('query')
+        return Book.objects.filter(
+            Q(title__icontains=query) | Q(title__icontains=query)
+        )
